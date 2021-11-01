d6d6955859c8db824bd24d524f07ee4e9bc74e4c
loulou <loulou@sun.net.tw>
2021-11-01 11:26:12 +0800

    push all

---
+++ src/main/java/com/example/receiptspringserver/ReceiptSpringServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.receiptspringserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptSpringServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceiptSpringServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/receiptspringserver/ReceiptSpringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.receiptspringserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptSpringServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
